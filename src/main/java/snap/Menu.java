70c01fce1e1cf2088fe7a34fd3748f9843334342
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-05-23 15:24:32 +0100

    grabbing from main

--- src/main/java/snap/Menu.java
+++
@@ -1,7 +0,0 @@
-package snap;
-
-public class Menu {
-    public static void main(String[] args) {
-        System.out.println("Snap Crackle Pop");
-    }
-}
---
+++ src/main/java/snap/MenuSnap.java
@@ -0,0 +1,6 @@
+package snap;
+
+public class MenuSnap {
+
+
+}
